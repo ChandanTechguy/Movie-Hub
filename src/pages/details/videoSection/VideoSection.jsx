f7372d41fb585efcbb6501e8f8eb113b9eb851a9
ChandanTechguy <109069555+ChandanTechguy@users.noreply.github.com>
2023-06-26 10:32:31 +0530

    Update VideoSection.jsx

--- src/pages/details/videoSection/VideoSection.jsx
+++ src/pages/details/videoSection/VideoSection.jsx
@@ -23,24 +23,26 @@ const VideoSection = ({ data, loading }) => {
     <div className="videosSection">
       <ContentWrapper>
         <div className="sectionHeading">Official Videos</div>
-        {loading ? (
-          <div className="videos">
-            {data?.results?.map((video) => {
-              <div key={video.id} className="videoItem"
-              onClick={()=>{
-                setVideoId(video.key)
-                setShow(true)
-              }}
-              >
-                <div className="videoThumbnail">
-                    <Img src={`https://img/youtube.com/vi/${video.key}/mqdefault.jpg`}/>
-                    <PlayIcon/>
-                </div>
-                <div className="videoTitle">
-                    {video.name}
-                </div>
-              </div>;
-            })}
+       {!loading ? (
+                    <div className="videos">
+                        {data?.results?.map((video) => (
+                            <div
+                                key={video.id}
+                                className="videoItem"
+                                onClick={() => {
+                                    setVideoId(video.key);
+                                    setShow(true);
+                                }}
+                            >
+                                <div className="videoThumbnail">
+                                    <Img
+                                        src={`https://img.youtube.com/vi/${video.key}/mqdefault.jpg`}
+                                    />
+                                    <PlayIcon />
+                                </div>
+                                <div className="videoTitle">{video.name}</div>
+                            </div>
+                        ))}
           </div>
         ) : (
           <div className="videoSkeleton">
